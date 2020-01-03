cdfb45deb73d8d87bf9cca9d7bd49b3492f92a5d
exiao <huaxiao@umich.edu>
2020-01-02 22:57:31 -0800

    add user model, users and sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,54 @@
+class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ['Invalid credentials'], status: 401
+        end
+    end
+
+    def destroy
+        if current_user
+            logout!
+            render json: {}
+        else
+            render json: {}, status: 404
+        end
+    end
+end
+
+# test creating a user
+# $.ajax({
+#     url:"api/users",
+#     method:"POST",
+#     data: {
+#         user:{
+#             username: 'a',
+#             email: "a@a.com",
+#             password: 'asdf1234'
+#         }
+#     }
+# }).then(res => console.log(res));
+
+# test signing out
+# $.ajax({
+#     url:"api/session",
+#     method:"DELETE"
+# }).then(res => console.log(res));
+
+# test signing in
+# $.ajax({
+#     url:"api/session",
+#     method:"POST",
+#     data: {
+#         user:{
+#             username: 'a',
+#             password: 'asdf1234'
+#         }
+#     }
+# }).then(res => console.log(res));
