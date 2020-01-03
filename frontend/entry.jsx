ca0d396ae873cc9455486d80834545c2ebc9dd68
exiao <huaxiao@umich.edu>
2020-01-02 23:19:40 -0800

    add yoda

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+let yoda = (<img src="https://media1.tenor.com/images/709f58a3fad3e65122487a8cc4f12490/tenor.gif?itemid=15739229"></img>)
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to Coinbase</h1>, root);
+    ReactDOM.render(yoda, root);
 });
