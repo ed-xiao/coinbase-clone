8ad3e4a8046892229610c4621cb7e2e69cac2804
exiao <huaxiao@umich.edu>
2020-01-03 15:07:16 -0800

    add bootstrap auth

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,7 +6,21 @@ import Root from './components/root';
 let yoda = (<img src="https://media1.tenor.com/images/709f58a3fad3e65122487a8cc4f12490/tenor.gif?itemid=15739229"></img>)
 
 document.addEventListener('DOMContentLoaded', () => {
-    const store = configureStore();
+    // const store = configureStore();
+
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
 
     //REMOVE LATER
     window.getState = store.getState;
