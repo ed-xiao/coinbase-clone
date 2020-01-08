b6f3f66ff5514e1c283d58158632740806cf7b30
exiao <huaxiao@umich.edu>
2020-01-07 23:16:30 -0800

    delete portfolios controller

--- app/controllers/portfolios_controller.rb
+++
@@ -1,2 +0,0 @@
-class PortfoliosController < ApplicationController
-end
--- test/controllers/portfolios_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PortfoliosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
