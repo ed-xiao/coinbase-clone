e6aa0ec0f84142a4d861639b5c841c13075e3e9d
exiao <huaxiao@umich.edu>
2020-01-10 14:37:17 -0800

    inital modal code

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,11 +13,14 @@ import NavContainer from '../components/nav/nav_container';
 // import PortContainer from '../components/portfolio/portfolio_container';
 import Welcome from '../components/welcome/welcome';
 import Footer from '../components/footer/footer';
+// import Modal from '../components/modal/modal';
 
 // update line 21 to be new portfolio container
 const App = () => (
     <div className='top'>
+        {/* {<Modal />} */}
         <Route path="/" component={NavContainer} />
+
         <Switch>
             <AuthRoute exact path='/signup' component={SignupFormContainer} />
             <AuthRoute exact path='/login' component={LoginFormContainer} />
