8f59e88237b3bb274ae8faa6b9c76f92ddd4054b
exiao <huaxiao@umich.edu>
2020-01-15 11:35:53 -0800

    finish backend for posting and deleting watchlists

--- app/controllers/api/watchlists_controller.rb
+++ app/controllers/api/watchlists_controller.rb
@@ -3,9 +3,24 @@ class Api::WatchlistsController < ApplicationController
         # @watched_cryptos = current_user.watched_cryptos
         @watchlists = current_user.watchlists
     end
+
     def create
+        @watchlist = Watchlist.new(watchlist_params)
+        @watchlist.user_id = current_user.id
+        if @watchlist.save
+            render json: ['success'], status: 200
+        else
+            render json: @watchlist.errors.full_messages, status: 422
+        end
     end
+
     def destroy
+        @watchlist = Watchlist.find(params[:id])
+        if @watchlist.destroy
+            render json: ['success'], status: 200
+        else
+            render json: @watchlist.errors.full_messages, status: 422
+        end
     end
 
     private
@@ -13,3 +28,20 @@ class Api::WatchlistsController < ApplicationController
         params.require(:watchlist).permit(:crypto_id)
     end
 end
+
+# test creating a watched crypto
+# $.ajax({
+#     url:"api/watchlists",
+#     method:"POST",
+#     data: {
+#         watchlist:{
+#             crypto_id: 3
+#         }
+#     }
+# }).then(res => console.log(res));
+
+# test deleting a watched crypto
+# $.ajax({
+#     url:"api/watchlists/4",
+#     method:"DELETE"
+# }).then(res => console.log(res));
