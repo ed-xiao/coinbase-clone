24ee1b342d87014f03e0e30c27caec0051632f11
exiao <huaxiao@umich.edu>
2020-01-15 10:44:05 -0800

    update Watchlist model

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,2 +1,8 @@
 class Watchlist < ApplicationRecord
+    validates :user_id, :crypto_id, presence: true
+    validates :crypto_id, uniqueness: { scope: :user_id,
+    message: 'User already has this crypto in their watchlist'}
+
+    belongs_to :user
+    belongs_to :crypto
 end
