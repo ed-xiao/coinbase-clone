f0344641385842d842659de4d816f866b7d5a88f
exiao <huaxiao@umich.edu>
2020-02-14 13:34:50 -0800

    update css to have header and footer stick to top and bottom and make .top 100 vh

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,8 +6,7 @@ import Root from './components/root';
 let yoda = (<img src="https://media1.tenor.com/images/709f58a3fad3e65122487a8cc4f12490/tenor.gif?itemid=15739229"></img>)
 
 document.addEventListener('DOMContentLoaded', () => {
-    // const store = configureStore();
-
+    
     let store;
     if (window.currentUser) {
         const preloadedState = {
