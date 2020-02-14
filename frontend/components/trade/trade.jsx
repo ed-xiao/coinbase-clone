baa2f5ec1d0dd7355aafdb45ee8217aaf2466ddc
exiao <huaxiao@umich.edu>
2020-02-14 01:11:26 -0800

    add translate to coin name in trade modal

--- frontend/components/trade/trade.jsx
+++ frontend/components/trade/trade.jsx
@@ -74,39 +74,47 @@ class Trade extends React.Component {
         // }
         let imageSource = window[this.state.symbol];
         return (
-            <div className='widget-trade'>
-                <div className='trade-top'>
-                    <div className='active' onClick={this.handleOrderClick("buy")}>Buy</div>
-                    <div onClick={this.handleOrderClick("sell")}>Sell</div>
-                    <div onClick={this.handleOrderClick("convert")}>Convert</div>
-                </div>
-                <div className='trade-middle'>
-                    <input type="number" placeholder='0 units' minLength='1' onChange={this.update('units')
-                        // onKeyPress={evt => evt.key === 'e' && evt.preventDefault()}
-                    }/>
-                    <div>
-                        {/* {errors go here} */}
-                    </div>
-                    <div className='trade-crypto-selector' onClick={this.openSelectModal}>
-                        <p>
-                            Buy
-                        </p>
-                        <div>
-                            <div>
-                                <img src={imageSource} />
-                            </div>
-                            <p>
-                                {translateName[this.state.name]}
-                            </p>
-                        </div>
-                    </div>
-                    <button type='button' onClick={this.handleClick}>
-                        {transaction_type.slice(0, 1).toUpperCase().concat(transaction_type.slice(1))} {this.state.name}
-                    </button>
-                </div>
-                <div className='trade-bottom'>
-                </div>
+          <div className="widget-trade">
+            <div className="trade-top">
+              <div className="active" onClick={this.handleOrderClick("buy")}>
+                Buy
+              </div>
+              <div onClick={this.handleOrderClick("sell")}>Sell</div>
+              <div onClick={this.handleOrderClick("convert")}>Convert</div>
             </div>
+            <div className="trade-middle">
+              <input
+                type="number"
+                placeholder="0 units"
+                minLength="1"
+                onChange={
+                  this.update("units")
+                  // onKeyPress={evt => evt.key === 'e' && evt.preventDefault()}
+                }
+              />
+              <div>{/* {errors go here} */}</div>
+              <div
+                className="trade-crypto-selector"
+                onClick={this.openSelectModal}
+              >
+                <p>Buy</p>
+                <div>
+                  <div>
+                    <img src={imageSource} />
+                  </div>
+                  <p>{translateName[this.state.name]}</p>
+                </div>
+              </div>
+              <button type="button" onClick={this.handleClick}>
+                {transaction_type
+                  .slice(0, 1)
+                  .toUpperCase()
+                  .concat(transaction_type.slice(1))}{" "}
+                {translateName[this.state.name]}
+              </button>
+            </div>
+            <div className="trade-bottom"></div>
+          </div>
         );
     }
 }
