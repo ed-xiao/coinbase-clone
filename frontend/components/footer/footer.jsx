c9409bd75976b8a24a60c420338bd592933629c4
exiao <huaxiao@umich.edu>
2020-02-23 13:53:23 -0800

    update footer links to open new tab

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,13 +5,19 @@ import React from 'react';
 
 class Footer extends React.Component {
     render() {
-        return(
-        <footer>
-            <a href="https://github.com/ed-xiao"><i className="fab fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/edward-xiao/"><i className="fab fa-linkedin"></i></a>
-            <a href="https://angel.co/edward-xiao-5"><i className="fab fa-angellist"></i></a>
-        </footer>
-        )
+        return (
+          <footer>
+            <a href="https://github.com/ed-xiao" target="_blank">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/edward-xiao/" target="_blank">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://angel.co/edward-xiao-5" target="_blank">
+              <i className="fab fa-angellist"></i>
+            </a>
+          </footer>
+        );
     }
 }
 
